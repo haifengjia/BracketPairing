45ecd69b3930295021768af2b61a4ee3b0055393
EthanJia <181716254@qq.com>
2021-04-12 13:43:02 +0800

    feat: first version pairing func

--- BracketPairing.py
+++ BracketPairing.py
@@ -3,17 +3,35 @@ from Stack import Stack
 
 
 def pairing(fileList, row, col, left='(', right=')'):
+    # FIXME: add the first row/col to start
     pos = 0
-    i = j = 0
+    i = 0
+    res = []
+    L = []
+    R = []
     brkStk = Stack()
     for line in fileList:
         # print(line)
         i += 1
+        j = 0
         for c in line:
             # print(c)
             j += 1
             if c == left:
-                brkStk.push(j)
+                brkStk.push(c)
+                L.append((i, j))
+            elif c == right:
+                if not brkStk.isEmpty():
+                    R.append((i, j))
+                    brkStk.pop()
+                else:
+                    L.append((-1, -1))
+                    R.append((i, j))
+                    continue
+            else:
+                continue
+    res = [L, R]
+    return res
 
 
 if __name__ == '__main__':
@@ -23,4 +41,5 @@ if __name__ == '__main__':
     fileList = open(fileName, 'r')
     content = fileList.readlines()
     # print(content)
-    pairing(content, 1, 1, '(', ')')
+    l = pairing(content, 1, 1, '(', ')')
+    print(l)
